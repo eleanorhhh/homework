0b5840d823fb7cb5570f631fd9d887e50553287e
eleanor <tinafa3222@gmail.coml>
2023-08-09 17:35:46 +0800

    Ch07_practice01

---
+++ Ch07_practice01.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ch07_practice01 {
+    public static void main(String[] args) throws IOException{
+        int array_size;
+        String str;
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("請輸入陣列大小(正整數):");
+        str = buf.readLine();
+        System.out.print("陣列a的長度為:"+str);
+        
+
+
+    }  
+}
