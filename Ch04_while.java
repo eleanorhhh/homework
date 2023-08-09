be575a5c8e9776681d619d6d2aab6ffb13bc3d22
eleanor <tinafa3222@gmail.coml>
2023-08-09 15:23:42 +0800

    Ch04_while

---
+++ Ch04_while.java
@@ -0,0 +1,12 @@
+public class Ch04_while {
+    public static void main(String[] args) {
+        int i=1 ,n=5, ans=1;
+        while(i<=n){
+            ans=2*ans;
+            System.out.println("2的"+i+"次方="+ans);
+            i++;
+
+        }
+
+    }
+}
