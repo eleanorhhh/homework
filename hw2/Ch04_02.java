db264e4dd713684484746024f0da84f78fbc8e03
eleanor <tinafa3222@gmail.coml>
2023-08-09 14:33:41 +0800

    Ch04_02

---
+++ hw2/Ch04_02.java
@@ -0,0 +1,12 @@
+package hw2;
+
+public class Ch04_02 {
+    public static void main(String[] args) {
+        int a = 15;
+        if (a % 2 == 0) // 如果可被2整除
+            System.out.println(a + "is an even number"); // 印出a為偶數
+        else
+            System.out.println(a + "is an odd number"); // 印出a為奇數
+
+    }
+}
