4c8323f4e5d4389c57046d145e749440e2343ac1
eleanor <tinafa3222@gmail.coml>
2023-08-11 14:59:45 +0800

    Swap

---
+++ Practice_swap02.java
@@ -0,0 +1,13 @@
+
+public class Practice_swap02 {
+    public static void main(String[] args) {
+        int a[] = { 1, 2, 3, 4 };
+        int b = a[0];
+        int c = a[1];
+        swap(b, c);
+    }
+
+    public static void swap(int* a, int* b) {
+
+    }
+}
