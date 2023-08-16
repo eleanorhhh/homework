cc1aaded5689d0cd8696a01740649ab3e6d281cd
eleanor <tinafa3222@gmail.coml>
2023-08-16 16:45:05 +0800

    MatrixCaculator.java

---
+++ Matrix.java
@@ -0,0 +1,27 @@
+public class Matrix {
+    public int row;
+    public int col;
+    public int[][] numbers;
+
+    // array 初始化 會跟外面的連動 所以要用 copy 不可以直接等於
+    public Matrix(int row, int col, int[][] numbers) {
+        this.row = row;
+        this.col = col;
+        this.numbers=new int[numbers.length][numbers[0].length];
+        for (int i = 0; i < numbers.length; i++) {
+            System.arraycopy(numbers[i], 0, this.numbers[i], 0, numbers[i].length);
+        }
+        
+        
+
+    }
+
+    public void print() {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.printf("%d\t", numbers[i][j]);
+            }
+            System.out.print("\n");
+        }
+    }
+}
---
+++ MatrixCaculator.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class MatrixCaculator {
+    public static void printmatrix(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        int i = 0, j = 0;
+        int length;
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("請輸入幾乘幾矩陣:");
+        System.out.println("x值:");
+        try {
+            i = Integer.parseInt(buf.readLine());
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        System.out.println("y值:");
+        try {
+            j = Integer.parseInt(buf.readLine());
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        System.out.println("輸入" + i + "乘" + j + "矩陣:");
+        int arr[][] = new int[i][j];
+
+        length = i * j;
+        int temp = length;
+        while (temp > 0) {
+            int index = length - temp;
+            int x = index / j;
+            int y = index % j;
+            System.out.printf("x:%d y:%d index=%d \n", x, y, index);
+
+            try {
+                System.out.printf("Please input %d number\n", index + 1);
+                arr[x][y] = Integer.parseInt(buf.readLine());
+
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+            temp--;
+        }
+        printmatrix(arr);
+        Matrix mat = new Matrix(i, j, arr);
+        arr[0][1] = 8;
+        mat.print();
+    }
+
+}
